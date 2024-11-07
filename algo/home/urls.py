7db4c61a89c613b84fa7fcf11f669d83725b8fec
Erfan-aminian <aminian158@gmail.com>
2024-11-07 14:59:58 +0330

    add css file, fcfs url

--- algo/home/urls.py
+++ algo/home/urls.py
@@ -3,4 +3,6 @@ from django.urls import path, include
 app_name = 'home'
 urlpatterns = [
     path('home/', views.HomeView.as_view(), name='home'),
+    path('fcfs/', views.FcfsView.as_view(), name='fcfs'),
+
 ]
--- algo/home/views.py
+++ algo/home/views.py
@@ -3,21 +3,29 @@ from django.views import View
 from .forms import AlgorithmForm
 from .models import AlgorithmModel
 from django.contrib import messages
+from django.http import HttpResponse
 # Create your views here.
 
 
 class HomeView(View):
     def get(self, request):
         form = AlgorithmForm()
-        return render(request, 'home/home.html',{'form': form})
+        return render(request, 'home/home.html', {'form': form})
     def post(self, request):
         form = AlgorithmForm(request.POST)
         if form.is_valid():
-            form.save()
+            selected = form.cleaned_data['option']
+            if selected=='1':
+                return redirect('home:fcfs')
             messages.success(request, 'you chioce', 'success')
+            form.save()
         else:
             form = AlgorithmForm()
-        return render(request, 'home/home.html',{'form': form})
+        return render(request, 'home/home.html', {'form': form})
+class FcfsView(View):
+    def get(self, request):
+        return HttpResponse('this page for fcfs algorithm.')
+
 
 
 
