aa74ae9dd85a09631f95f961f453038829157d14
Erfan-aminian <aminian158@gmail.com>
2024-11-16 22:18:27 +0330

    add dark template for fcfs,count

--- algo/home/forms.py
+++ algo/home/forms.py
@@ -19,7 +19,7 @@ class DynamicForm(forms.Form):
 
         # اضافه کردن فیلدها بر اساس تعداد وارد شده در فرم اول
         for i in range(field_count):
-            self.fields[f'field_{i}'] = forms.CharField(label=f'Enter Process:{i + 1}')
+            self.fields[f'field_{i}'] = forms.IntegerField(label=f'Enter Process:{i + 1}')
             self.fields[f'number {i}'] = forms.IntegerField(label=f'Enter Number:{i + 1}')
 
 
