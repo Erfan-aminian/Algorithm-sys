d5ccf1ff266b17e0c52fa2bd2b6fe846a2caf1be
Erfan-aminian <aminian158@gmail.com>
2024-12-12 20:48:06 +0330

    change

--- algo/home/forms.py
+++ algo/home/forms.py
@@ -11,14 +11,6 @@ class AlgorithmForm(forms.ModelForm):
             'option': forms.RadioSelect
         }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # اگر الگوریتم Round Robin انتخاب شده باشد، فیلد زمان کوانتوم را اضافه کن
-        if self.instance and self.instance.option == 'RR':
-            self.fields['quantum'] = forms.IntegerField(min_value=1, required=True, label='Quantum Time', initial=5)
-        else:
-            # اگر انتخاب دیگری باشد، فیلد quantum را مخفی کن
-            self.fields.pop('quantum', None)
 
 
 class FirstForm(forms.Form):
@@ -29,6 +21,7 @@ class DynamicForm(forms.ModelForm):
     class Meta:
         model = DynamicProcessModel
         fields = ('process_name', 'arrival_time', 'burst_time', 'quantum')
+
 
 
 def create_dynamic_process_formset(field_count=0):
